f506cd143151745ca4745e2a5287ae9b75b0dee7
saru-16 <sarunagavalar9441@gmail.com>
2024-09-24 20:56:49 +0530

    Create squareservlet.java

---
+++ squareservlet.java
@@ -0,0 +1,17 @@
+package com.serveletdemo1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class squareservlet extends HttpServlet {
+	public void service(HttpServletRequest req ,HttpServletResponse res)throws IOException{
+		int result=Integer.parseInt(req.getParameter("ans"));
+		result=result*result;
+		PrintWriter out=res.getWriter();
+		out.print("square "+result);
+	}
+}
